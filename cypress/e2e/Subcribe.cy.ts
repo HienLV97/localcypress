9a90a318d15f114d514050deb5e3bac3f4b9850b
Le Vinh Hien <levinhhien@Les-MacBook-Air.local>
2022-11-04 11:23:21 +0700

    test chi vy

--- cypress/e2e/Subcribe.cy.ts
+++ cypress/e2e/Subcribe.cy.ts
@@ -3,29 +3,9 @@ describe("Newsletter Subscribe Form", () => {
         cy.visit("http://localhost:3000")
         //cy.viewport(1440, 812)
     })
-    // context("hero section", () => {
-    //     it("allow users to subscribe to the email list", () => {
-    //         cy.getByData("email-input").type("abc@a.com")
-    //         cy.getByData("submit-button").click()
-    //         cy.getByData("success-message").should("exist").contains("abc@a.com")
-    //     })
-    //     it("does NOT allow a invalid email address", () => {
-    //         cy.getByData("email-input").type("abc")
-    //         cy.getByData("submit-button").click()
-    //         cy.getByData("success-message").should("not.exist")
-    //     })
-    // })
-    // context("Test button", () => {
-    //     it("Testcase 1:",() =>{
-    //         cy.getByData("email-input").type("test")
-    //         cy.getByData("submit-button").click()
-    //         cy.getByData("succes-message").should("not.exist")           
-    //     })
-    // })
-    context("Test case 2",() => {
-
-        it("TC1",() =>{
-            cy.getByData("course-2").find("a")//.eq(3).click()       
+    context("hero section", () => {
+        it("allow users to subscribe to the email list", () => {
+            cy.getByData("email-input").type("abc@a.com")
         })
     })
 })
