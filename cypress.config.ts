cd8c747d86b2ad090c38cd3ba710670a516b189a
HienLV97 <levinhhien2603@gmail.com>
2023-05-16 11:33:05 +0700

    t

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,10 +8,23 @@ export default defineConfig({
     env:{
      
     },
-    baseUrl:'https://kamora:iamafriend@writersperhour.dev/',
+    baseUrl:'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     
     },
   },
 });
+
+//   e2e:  
+//   {
+//     env:{
+     
+//     },
+//     baseUrl:'https://kamora:iamafriend@writersperhour.dev/',
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+    
+//     },
+//   },
+// });
