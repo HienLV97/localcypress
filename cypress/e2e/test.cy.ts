e7410ff26a8a0c0c97ce9b7a306468072fcc6fc5
Hienlv97 <levinhhien2603@gmail.com>
2023-06-19 21:51:32 +0700

    a

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -5,10 +5,17 @@ describe("home page", () => {
   it('use requests to navigation bar links', () => {
     cy.visit('/')
     cy.get("h1").contains("Test")
-    cy.get('[data-test="email-input"]').type("abc.def@mail")
-    cy.getDatatest("submit-button").then(($btn) => {
-      const cls = $btn.attr("class")
-      cy.wrap($btn).click().should("contain","Subscribe")
+    // cy.get('[data-test="email-input"]').type("abc.def@mail")
+    cy.get('[data-test="email-input"]').then(($input) => {
+      cy.wrap($input).type("hien@gmail.com")
     })
+    /*  cy.getDatatest("submit-button").then(($btn) => {
+       cy.wrap($btn).click().should("contain","Subscribe")
+     })
+  */
+    cy.getDatatest("submit-button")
+      .as("Sub")
+    cy.get("@Sub")
+      .click()
   })
 })
